2ed297b9c2d451799d144c4387aa528d434be8f9
David Reed <david.reed.c@gmail.com>
2014-07-10 13:09:53 -0400

    Added Jinja templating to make my life easier

---
+++ convert.py
@@ -0,0 +1,26 @@
+from jinja2 import Environment, FileSystemLoader
+
+loader = FileSystemLoader('templates')
+
+env = Environment(loader=loader)
+
+
+def convert_file(fname):
+	template = env.get_template(fname)
+
+	a = template.render(active=fname)
+
+	fid = open(fname, 'w')
+	fid.write(a)
+	fid.close()
+
+convert_file('index.html')
+convert_file('transform.html')
+convert_file('define.html')
+convert_file('explore.html')
+convert_file('getdata.html')
+convert_file('create.html')
+convert_file('enrich.html')
+
+convert_file('getting_started.html')
+
